e60e775391267cc3b3cbc68ba5c6a551fc18d7cc
alighavamipour <135832882+alighavamipour@users.noreply.github.com>
2025-11-16 20:58:44 +0330

    Update utils_soundcloud.py

--- utils_soundcloud.py
+++ utils_soundcloud.py
@@ -1,1 +1,15 @@
+# utils_soundcloud.py
+import yt_dlp
 
+def download_soundcloud(url):
+    out = "data/sound.mp3"
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": out
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return out
