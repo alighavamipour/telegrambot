abd671744fdd0e7acf5a72bcbbc002c855c187e0
alighavamipour <135832882+alighavamipour@users.noreply.github.com>
2025-11-22 22:21:56 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import re, os, logging
 from config import DOWNLOAD_PATH, REQUIRED_CHANNELS, CHANNEL_ID
 from yt_dlp import YoutubeDL
-from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, COMM, TCON, ID3NoHeaderError
 
 logger = logging.getLogger(__name__)
 os.makedirs(DOWNLOAD_PATH, exist_ok=True)
@@ -22,59 +21,55 @@ def user_display_name(user):
     ln = user.last_name or ""
     return (fn + (" " + ln if ln else "")).strip() or "ناشناس"
 
-
 # ------------------- MAKE CHANNEL CAPTION -------------------
 def make_channel_caption(channel_id=None):
     ch = channel_id or CHANNEL_ID
     return f"https://t.me/{ch.lstrip('@')}"
-
 
 # ------------------- CHECK MEMBERSHIP -------------------
 def check_membership(bot, user_id):
     try:
         for ch in REQUIRED_CHANNELS:
             member = bot.get_chat_member(ch, user_id)
+            logger.info("User %s status in %s: %s", user_id, ch, member.status)
             if member.status not in ['left', 'kicked']:
                 return True
         return False
     except Exception as e:
-        logger.error("Membership check failed: %s", e)
+        logger.exception("Membership check failed")
         return False
 
-
-#################################
-#     NEW DOWNLOADER
-#################################
-
+# ------------------- YTDLP DOWNLOAD -------------------
 def ytdlp_download(url, outdir, quality=None, audio_only=False):
     os.makedirs(outdir, exist_ok=True)
-
     opts = {
         "outtmpl": f"{outdir}/%(title)s.%(ext)s",
         "quiet": True,
         "no_warnings": True,
-        "noplaylist": False
+        "noplaylist": True,
     }
 
     if audio_only:
         opts['format'] = 'bestaudio/best'
-        opts['postprocessors'] = [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3"
-        }]
+        opts['postprocessors'] = [{"key": "FFmpegExtractAudio","preferredcodec": "mp3"}]
     else:
         if quality:
             opts['format'] = f"bestvideo[height<={quality}]+bestaudio/best"
         else:
             opts['format'] = "bestvideo+bestaudio/best"
 
-    with YoutubeDL(opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        file = ydl.prepare_filename(info)
+    try:
+        with YoutubeDL(opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+            if audio_only:
+                filename = os.path.splitext(filename)[0] + ".mp3"
+        return filename, info
+    except Exception as e:
+        logger.exception("Download failed for %s", url)
+        raise e
 
-    return file, info
-
-
+# ------------------- GET THUMBNAIL -------------------
 def get_thumbnail(info):
     try:
         if "thumbnail" in info:
