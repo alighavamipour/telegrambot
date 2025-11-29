23fbba92ba6854b65be37e90549087129d2e6aa6
alighavamipour <135832882+alighavamipour@users.noreply.github.com>
2025-11-29 23:14:21 +0330

    Update session_maker.py

--- session_maker.py
+++ session_maker.py
@@ -3,7 +3,7 @@ import asyncio
 import os
 
 API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH"))
+API_HASH = os.getenv("API_HASH")
 
 async def main():
     async with Client("gen_session", api_id=API_ID, api_hash=API_HASH) as app:
